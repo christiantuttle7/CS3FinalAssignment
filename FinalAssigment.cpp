c183e956cd93e315c22996b205e6cb8852c0ac7b
NoahFoli <noahf2017@gmail.com>
2025-05-06 14:15:49 -0600

    added switch case skeleton code.

--- FinalAssigment.cpp
+++ FinalAssigment.cpp
@@ -11,6 +11,25 @@ int main(){
    Hashing Object;
    Object.testHashFunctions();
 
+   //main menue //switch case for choice -noah
+   int user_input =0;
+   cout << "MENU"<<endl;
+   cout<<"------------------" <<endl;
+
+   switch (user_input)
+   {
+      case 1:  
+
+      case 2:
+
+      case 3:
+
+      case 4:
+
+      default:
+
+   }
+
 
    return 0;
    
