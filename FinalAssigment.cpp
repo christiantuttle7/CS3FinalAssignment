2760cd4d4740db599883e3e2f0e364ce55eb5652
NoahFoli <noahf2017@gmail.com>
2025-05-06 14:39:07 -0600

    updated switch case

--- FinalAssigment.cpp
+++ FinalAssigment.cpp
@@ -5,30 +5,57 @@
 #include "Hashing.h"
 using namespace std;
 
-int main(){
+int main()
+{
     
    
-   Hashing Object;
-   Object.testHashFunctions();
+Hashing Object;
+Object.testHashFunctions();
 
    //main menue //switch case for choice -noah
-   int user_input =0;
-   cout << "MENU"<<endl;
-   cout<<"------------------" <<endl;
+int user_input = -1;
 
-   switch (user_input)
-   {
-      case 1:  
+while (user_input != 0)
+{
 
-      case 2:
+      cout << "Menu\n";
+      cout<<"------------------\n";
+      cout << "1. Display 80 Most Frequent Words\n";
+      cout << "2. Display 80 Least Frequent Words\n";
+      cout << "3. Count Sentences in the text\n";
+      cout << "4. Prompt User for Word Search in Engineer's Thumb\n";
+      cout << "5. Run All Performance Reports\n";
+      cout << "0. Exit\n";
+      cout << "Enter your choice: ";
+      cin >> user_input;
 
-      case 3:
+      switch (user_input)
+      {
+         case 0:
+            cout << "Goodbye!" << endl;
+            break;
+            
+         case 1: 
+         
+            break;
 
-      case 4:
+         case 2:
 
-      default:
+            break;
 
-   }
+         case 3:
+
+            break;
+
+         case 4:
+
+            break;
+
+         default:
+            cout << "this is an invalid option" << endl;
+
+      }
+}
 
 
    return 0;
